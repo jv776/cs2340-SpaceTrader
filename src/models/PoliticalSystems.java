73438ccd7e7cb63e9eab632c5cba51cf17996027
2baud <hardy.taylor.j@gmail.com>
2014-09-18 12:33:29 -0400

    Added Political System enum

---
+++ src/models/PoliticalSystems.java
@@ -0,0 +1,25 @@
+package models;
+
+/**
+ * Created by Taylor on 9/18/14.
+ */ //Moved the TechLevels enum out of Universe so that other classes won't have to
+//access it as Universe.TechLevel
+enum PoliticalSystems {
+    ANARCHY,
+    CAPITALIST_STATE,
+    COMMUNIST_STATE,
+    CONFEDERACY,
+    CORPORATE_STATE,
+    CYBERNETIC_STATE,
+    DEMOCRACY,
+    DICTATORSHIP,
+    FASCIST_STATE,
+    FEUDAL_STATE,
+    MILITARY_STATE,
+    MONARCHY,
+    PACIFIST_STATE,
+    SOCIALIST_STATE,
+    STATE_OF_SATORI,
+    TECHNOCRACY,
+    THEOCRACY
+}
