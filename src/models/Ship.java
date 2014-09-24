cff591c77fef66256f006e05ed5a5c6012120bce
John Varela <john.varela@gatech.edu>
2014-09-24 13:10:36 -0400

    Combined ship classes into one enum and made temporary market GUI slightly more professional-looking for demo

--- src/models/Ship.java
+++ src/models/Ship.java
@@ -9,7 +9,16 @@ package models;
  *
  * @author John Varela
  */
-abstract class Ship {
+public enum Ship {
+    Flea (10, 0, 0, 0, 1, 20, TechLevel.EARLY_INDUSTRIAL, 1, 2000, 5, 2, 25, 1),
+    Gnat (15, 1, 0, 1, 1, 14, TechLevel.INDUSTRIAL, 2, 10000, 50, 28, 100, 1),
+    Firefly (20, 1, 1, 1, 1, 17, TechLevel.INDUSTRIAL, 3, 25000, 75, 20, 100,
+            1),
+    Mosquito (15, 2, 1, 1, 1, 13, TechLevel.INDUSTRIAL, 5, 30000, 100, 20, 100,
+                1),
+    BumbleBee (25, 1, 2, 2, 2, 15, TechLevel.INDUSTRIAL, 7, 60000, 125, 15, 100,
+                1);
+    
     private final int cargoCapacity;
     //private Weapon[] weapons;
     //private Shield[] shields;
@@ -20,13 +29,13 @@ abstract class Ship {
     private final TechLevel minTechLevelToBuy;
     private final int fuelCost;
     private final int price;
-    private int bounty; //I'm not sure what this does...
-    private final int occurrence; //Once again not sure what this affects
+    private int bounty; //I'm not sure what this does
+    private int occurrence; //Once again not sure what this affects
     private final int maxHullStrength;
     private int hullStrength;
     private final int repairCost;
     
-    public Ship(int cargoHolds, int weaponSlots, int shieldSlots,
+    private Ship(int cargoHolds, int weaponSlots, int shieldSlots,
             int gadgetSlots, int crewMembers, int fuelTankSize,
             TechLevel minLevel, int fuelTankCost, int shipPrice, int shipBounty,
             int shipOccurrence, int shipHullStrength, int shipRepairCost) {
