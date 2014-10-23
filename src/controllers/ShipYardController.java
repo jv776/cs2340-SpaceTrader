c77259564e6ba0354b9a5779d56036b73da59b84
Roi Atalla <ratalla3@gatech.edu>
2014-10-23 18:02:49 -0400

    Added skeleton ShipYardController.java and ShipYard.fxml

---
+++ src/controllers/ShipYardController.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * @author Roi Atalla
+ */
+public class ShipYardController implements Initializable {
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        
+    }
+}
